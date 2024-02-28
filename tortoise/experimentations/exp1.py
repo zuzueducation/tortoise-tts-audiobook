f25f5d398d72aedcb0fa7865519a3a8b2b81280a
JoffreyCesbron <44499758+JoffreyCesbron@users.noreply.github.com>
2024-02-28 18:15:25 +0100

    adding more female voices

--- tortoise/experimentations/exp1.py
+++ tortoise/experimentations/exp1.py
@@ -1,19 +1,27 @@
 from tortoise.custom_models import Experimentation
-from .custom_stories import *
+from custom_stories import *
 from tortoise.custom_api import generate_sentence_and_save
 
 if __name__ == "__main__":
     experimentations = [
 
 
-        # voices
-        Experimentation("_bk_harp_006115", story_3, "fast", tts_init={"kv_cache": True, "half": True}, parameters={}),
-        Experimentation("_bk_peng_004302", story_3, "fast", tts_init={"kv_cache": True, "half": True}, parameters={}),
-        Experimentation("_bk_podm_004281", story_3, "fast", tts_init={"kv_cache": True, "half": True}, parameters={}),
-        Experimentation("_bk_reco_000784", story_3, "fast", tts_init={"kv_cache": True, "half": True}, parameters={}),
-        Experimentation("_jenna_rose_stein", story_3, "fast", tts_init={"kv_cache": True, "half": True}, parameters={}),
-        Experimentation("_tina_merryman", story_3, "fast", tts_init={"kv_cache": True, "half": True}, parameters={}),
-        Experimentation("_tracey_rose", story_3, "fast", tts_init={"kv_cache": True, "half": True}, parameters={}),
+        # voice 1
+        Experimentation("_amy_faal", story_3, "fast", tts_init={"kv_cache": True, "half": True}, parameters={}),
+        Experimentation("_anna_stephan", story_3, "fast", tts_init={"kv_cache": True, "half": True}, parameters={}),
+        Experimentation("_chandrika_chlevi", story_3, "fast", tts_init={"kv_cache": True, "half": True}, parameters={}),
+        Experimentation("_emma_powel", story_3, "fast", tts_init={"kv_cache": True, "half": True}, parameters={}),
+        Experimentation("_emma_powel_long", story_3, "fast", tts_init={"kv_cache": True, "half": True}, parameters={}),
+        Experimentation("_sally_walker", story_3, "fast", tts_init={"kv_cache": True, "half": True}, parameters={}),
+
+        # voice 2
+        Experimentation("_amy_faal", story_3, "fast", tts_init={"kv_cache": True, "half": True}, parameters={"temperature":.2, "top_p": .1}),
+        Experimentation("_anna_stephan", story_3, "fast", tts_init={"kv_cache": True, "half": True}, parameters={"temperature":.2, "top_p": .1}),
+        Experimentation("_chandrika_chlevi", story_3, "fast", tts_init={"kv_cache": True, "half": True}, parameters={"temperature":.2, "top_p": .1}),
+        Experimentation("_emma_powel", story_3, "fast", tts_init={"kv_cache": True, "half": True}, parameters={"temperature":.2, "top_p": .1}),
+        Experimentation("_emma_powel_long", story_3, "fast", tts_init={"kv_cache": True, "half": True}, parameters={"temperature":.2, "top_p": .1}),
+        Experimentation("_sally_walker", story_3, "fast", tts_init={"kv_cache": True, "half": True}, parameters={"temperature":.2, "top_p": .1}),
+
 
     ]
 
